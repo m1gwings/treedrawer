2cafaea0c1c06f7715b31ade1291dee65a656a8e
Cristiano Migali <cristiano.migali@gmail.com>
2020-09-01 00:28:42 +0200

    export addBoxAround in a separate function

--- tree/stringify.go
+++ tree/stringify.go
@@ -20,37 +20,9 @@ func stringify(t *Tree) *drawer.Drawer {
 		if err != nil {
 			log.Fatal(fmt.Errorf("error while drawing val with no children: %v", err))
 		}
-		err = d.DrawRune('╭', 0, 0)
+		err = addBoxAround(d, 0, 0, dValW+1, dValH+1)
 		if err != nil {
-			log.Fatal(fmt.Errorf("error while drawing ╭ with no children: %v", err))
-		}
-		err = d.DrawRune('╮', dValW+1, 0)
-		if err != nil {
-			log.Fatal(fmt.Errorf("error while drawing ╮ with no children: %v", err))
-		}
-		err = d.DrawRune('╰', 0, dValH+1)
-		if err != nil {
-			log.Fatal(fmt.Errorf("error while drawing ╰ with no children: %v", err))
-		}
-		err = d.DrawRune('╯', dValW+1, dValH+1)
-		if err != nil {
-			log.Fatal(fmt.Errorf("error while drawing ╯ with no children: %v", err))
-		}
-		for x := 1; x <= dValW; x++ {
-			for yMul := 0; yMul <= 1; yMul++ {
-				err = d.DrawRune('─', x, yMul*(dValH+1))
-				if err != nil {
-					log.Fatal(fmt.Errorf("error while drawing ─ with no children: %v", err))
-				}
-			}
-		}
-		for y := 1; y <= dValH; y++ {
-			for xMul := 0; xMul <= 1; xMul++ {
-				err = d.DrawRune('│', xMul*(dValW+1), y)
-				if err != nil {
-					log.Fatal(fmt.Errorf("error while drawing │ with no children: %v", err))
-				}
-			}
+			log.Fatal(fmt.Errorf("error while adding box with no children: %v", err))
 		}
 		return d
 	}
@@ -130,4 +102,50 @@ func stringify(t *Tree) *drawer.Drawer {
 		log.Fatal(fmt.Errorf("error while drawing right child: %v", err))
 	}
 	return d
+}
+
+func addBoxAround(d *drawer.Drawer, startX, startY, endX, endY int) error {
+	if startX < 0 || startY < 0 || endX < 0 || endY < 0 {
+		return fmt.Errorf("can't draw on negative coordinates %d %d %d %d", startX, startY, endX, endY)
+	}
+	if startX > endX || startY > endY {
+		return fmt.Errorf("start should be before end %d %d %d %d", startX, startY, endX, endY)
+	}
+	dW, dH := d.Dimens()
+	if endX >= dW || endY >= dH {
+		return fmt.Errorf("end overflows the drawer with dimes %d %d, %d %d %d %d", dW, dH, startX, startY, endX, endY)
+	}
+	err := d.DrawRune('╭', startX, startY)
+	if err != nil {
+		return fmt.Errorf("error while drawing ╭: %v", err)
+	}
+	err = d.DrawRune('╮', endX, startY)
+	if err != nil {
+		return fmt.Errorf("error while drawing ╮: %v", err)
+	}
+	err = d.DrawRune('╰', startX, endY)
+	if err != nil {
+		return fmt.Errorf("error while drawing ╰: %v", err)
+	}
+	err = d.DrawRune('╯', endX, endY)
+	if err != nil {
+		return fmt.Errorf("error while drawing ╯: %v", err)
+	}
+	for x := startX + 1; x < endX; x++ {
+		for yMul := 0; yMul <= 1; yMul++ {
+			err = d.DrawRune('─', x, yMul*(endY-startY)+startY)
+			if err != nil {
+				return fmt.Errorf("error while drawing ─: %v", err)
+			}
+		}
+	}
+	for y := startY + 1; y < endY; y++ {
+		for xMul := 0; xMul <= 1; xMul++ {
+			err = d.DrawRune('│', xMul*(endX-startX)+startX, y)
+			if err != nil {
+				return fmt.Errorf("error while drawing │: %v", err)
+			}
+		}
+	}
+	return nil
 }
